397ca2e5b2803105fcdf33201dbe281ada3b05f8
hoangmirs <hoang.mirs@gmail.com>
2017-07-13 23:47:51 +0700

    Add coreUI theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,16 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
-import Vue from 'vue'
-import App from './app.vue'
+console.log("Hello World from Webpacker")
+import Vue from "vue"
+import router from "./router"
+import App from "./App"
 
-document.addEventListener('turbolinks:load', () => {
-  const app = new Vue(App).$mount('app')
-  console.log(app)
+document.addEventListener("turbolinks:load", () => {
+  new Vue({
+    el: "app",
+    router,
+    template: "<App/>",
+    components: { App }
+  })
 })
